96526b86401303ec78aba3f52720aaf7095a56a3
Jsbk1999 <jsoriano1499@gmail.com>
2021-04-11 00:20:11 -0400

    finished party.py

---
+++ party.py
@@ -0,0 +1,9 @@
+userinput = input ("Let's party! How long until the party? (Give me a number.) ")
+usernum = int(userinput, 10)
+if (usernum < 1):
+	print ("PARTY NOW!!!")
+else:
+	for i in range (usernum, 0, - 1):
+		print(i)
+		if (i == 1):
+			print("PARTY TIME!")

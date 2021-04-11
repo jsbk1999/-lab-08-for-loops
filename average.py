3eac89ecae7a64258731c3d703959cd1c061904a
Jsbk1999 <jsoriano1499@gmail.com>
2021-04-11 00:24:17 -0400

    finished average.py

---
+++ average.py
@@ -0,0 +1,6 @@
+num1 = input("Enter a number please...")
+num2 = input("Enter another number please... ")
+num3 = input("Enter a third number please... ")
+num4 = input("Enter final number please...")
+average = (int(num1) + int(num2) + int(num3) + int(num4)) /4
+print("The average is: " + str(average))
